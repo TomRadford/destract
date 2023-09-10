b7b4d74bd55b03ea42b1557a1eac4c7cccb9b8ba
Tom Radford <tom@theradford.com>
2023-09-10 14:27:43 +0200

    error handling for thought not found

--- src/routes/remember/[id]/+page.server.ts
+++ src/routes/remember/[id]/+page.server.ts
@@ -1,8 +1,13 @@
 import prisma from '$lib/prisma.js';
+import { error } from '@sveltejs/kit';
 
 export const load = async ({ params }) => {
 	const { id } = params;
 	const thought = await prisma.thought.findUnique({ where: { id } });
-	await prisma.thought.delete({ where: { id } });
+	try {
+		await prisma.thought.delete({ where: { id } });
+	} catch {
+		throw error(500, { message: 'This thought has vanished 💨' });
+	}
 	return { thoughtContent: thought?.content };
 };
