c9299c6c5c92e700778010d33ae019dfbca830fc
Tom Radford <tom@theradford.com>
2023-09-10 14:28:02 +0200

    add motivation delay

--- src/routes/remember/[id]/+page.svelte
+++ src/routes/remember/[id]/+page.svelte
@@ -3,16 +3,19 @@
 
 	export let data;
 	const getMotivation = async () => {
+		await new Promise((fulfil) => {
+			setTimeout(fulfil, 2000);
+		});
 		const res = await fetch('/api/motivation');
 		return res.json();
 	};
 </script>
 
-<div class="flex flex-col items-center h-full gap-96">
+<div class="flex flex-col items-center h-full gap-24">
 	<h1 class="text-4xl font-bold text-center">{data.thoughtContent}</h1>
 	{#await getMotivation() then motivation}
-		<p transition:fade class=" font-thin flex-1">
-			{motivation}
+		<p transition:fade class=" font-thin flex-1 text-center">
+			{motivation}!
 		</p>
 	{/await}
 </div>
