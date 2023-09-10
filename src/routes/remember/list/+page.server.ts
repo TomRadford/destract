8d16d9091d81fa6d8ff9f64faed70fe4ecec0375
Tom Radford <tom@theradford.com>
2023-09-10 14:28:44 +0200

    redirect list on no thoughts

--- src/routes/remember/list/+page.server.ts
+++ src/routes/remember/list/+page.server.ts
@@ -1,8 +1,13 @@
 import prisma from '$lib/prisma.js';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ parent }) => {
 	const { user } = await parent();
 	const thoughts = await prisma.thought.findMany({ where: { userId: user.id } });
+
+	if (!thoughts.length) {
+		throw redirect(300, '/');
+	}
 
 	return {
 		thoughts
--- src/routes/remember/list/+page.svelte
+++ src/routes/remember/list/+page.svelte
@@ -3,16 +3,16 @@
 	export let data;
 </script>
 
-<div class="w-full flex justify-center">
-	<div class="w-max">
+<div class="flex flex-col items-center">
+	<div class="">
 		<h1 class="font-bold text-4xl text-center">Shweet! What would you like to remember?</h1>
 		<p class="text-right font-extralight">...remember, once you remember, I’ll forget 😌</p>
 	</div>
+	<ul class="mt-4 flex flex-col w-96 items-center gap-3">
+		{#each data.thoughts as thought}
+			<Button className="text-opacity-100" type="link" href="/remember/{thought.id}"
+				>{thought.hint}</Button
+			>
+		{/each}
+	</ul>
 </div>
-<ul class="mt-4 w-full flex flex-col items-center gap-3">
-	{#each data.thoughts as thought}
-		<Button className="text-opacity-100" type="link" href="/remember/{thought.id}"
-			>{thought.hint}</Button
-		>
-	{/each}
-</ul>
