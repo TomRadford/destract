e80820c02cb53713f9723288c27a2389dd21692a
Tom Radford <tom@theradford.com>
2023-09-10 14:27:11 +0200

    add error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="text-center flex flex-col gap-4">
+	<h1 class="font-semibold text-2xl">WOOPS! 💩</h1>
+	<p class="font-extralight">
+		{$page.error?.message}
+	</p>
+</div>
