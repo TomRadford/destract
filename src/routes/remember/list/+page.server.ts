c291d35666816308cda785575ec1eb17a0380410
Tom Radford <tom@theradford.com>
2023-09-12 22:15:50 +0200

    add canRemember check: - redirect user if they havent answered the checks

--- src/routes/remember/list/+page.server.ts
+++ src/routes/remember/list/+page.server.ts
@@ -1,13 +1,20 @@
 import prisma from '$lib/prisma.js';
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export const load = async ({ parent }) => {
 	const { user } = await parent();
-	const thoughts = await prisma.thought.findMany({ where: { userId: user.id } });
-
-	if (!thoughts.length) {
-		throw redirect(300, '/');
+	const thoughtCount = await prisma.thought.count({ where: { userId: user.id } });
+	if (!thoughtCount) {
+		throw redirect(307, '/');
 	}
+
+	if (!user.canRemember) {
+		throw error(401, {
+			message: 'Naughty! You need to answer the questions first! 🫢'
+		});
+	}
+
+	await prisma.user.update({ where: { id: user.id }, data: { canRemember: false } });
 
 	return {
 		thoughts
