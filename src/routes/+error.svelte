cd6b4e023016448d65e84b354cab52589fec6af2
Tom Radford <tom@theradford.com>
2023-09-12 22:48:32 +0200

    make errors bigger

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,14 +12,14 @@
 </script>
 
 <div class="text-center flex flex-col gap-4">
-	<h1 class="font-semibold text-2xl">WOOPS! 💩</h1>
+	<h1 class="font-bold text-4xl">WOOPS! 💩</h1>
 	{#if $page.status === 404}
-		<span class="font-extralight">
+		<span class="font-semibold text-2xl">
 			<p>Hmmm...</p>
 			<p>I can't seem to find that page 🤷‍♂️</p>
 		</span>
 	{:else}
-		<p class="font-extralight">
+		<p class="font-semibold text-2xl">
 			{$page.error?.message}
 		</p>
 	{/if}
