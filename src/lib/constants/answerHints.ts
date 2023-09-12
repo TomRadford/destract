1a170eb550d5ac235889d085f3ce925451ae43e8
Tom Radford <tom@theradford.com>
2023-09-12 22:48:11 +0200

    cleanup hints

--- src/lib/constants/answerHints.ts
+++ src/lib/constants/answerHints.ts
@@ -1,9 +1,9 @@
 export const answerHints = [
 	'Aww hell nah!',
 	'Yessir!',
-	'Naa dawg!',
-	'Fo sho!',
-	'Yeah fam!',
-	'Prob not lol',
-	'I rate so dawg'
+	'No sir!',
+	'For sure!',
+	'Yeah!',
+	'Nope!',
+	'I rate so!'
 ];
